8535fc40b4c1cc118b4c3d25551e422232e49bd0
曹艳玲 <caoyanling@caoyanlingdeMacBook-Pro.local>
2020-07-29 16:10:55 +0800

    融媒云小程序

---
+++ pages/login/index.js
@@ -0,0 +1,17 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  bindViewTap: function() {
+    wx.switchTab({
+      url: '/pages/index/index',
+    })
+  },
+})
